47f43fe0d6caf8d889c3267ac9489fc36b15eb0e
Adarsh Akshat <Adarsha@peachpay.app>
2024-04-04 12:30:53 +0530

    Update readme

--- src/cpt/AA_Awesome_Post_Type.php
+++ src/cpt/AA_Awesome_Post_Type.php
@@ -1,7 +1,7 @@
 <?php
 
 
-namespace Awesome_Assesment\cpt;
+namespace Awesome_Assesment\Cpt;
 
     class AA_Awesome_Post_Type
     {
@@ -11,29 +11,9 @@ namespace Awesome_Assesment\cpt;
 
         }
 
-        public function register()
+        public function register() :void
         {
-            $args = array(
-                'labels' => array(
-                    'name'               => _x( 'Awesomes', 'post type general name', 'awesome-assesment' ),
-                    'singular_name'      => _x( 'Awesome', 'post type singular name', 'awesome-assesment' ),
-                    'menu_name'          => _x( 'Awesomes', 'admin menu', 'awesome-assesment' ),
-                    'name_admin_bar'     => _x( 'Awesome', 'add new on admin bar', 'awesome-assesment' ),
-                    'add_new'            => _x( 'Add New', 'awesome', 'awesome-assesment' ),
-                    'add_new_item'       => __( 'Add New Awesome', 'awesome-assesment' ),
-                    'new_item'           => __( 'New Awesome', 'awesome-assesment' ),
-                    'edit_item'          => __( 'Edit Awesome', 'awesome-assesment' ),
-                    'view_item'          => __( 'View Awesome', 'awesome-assesment' ),
-                    'all_items'          => __( 'All Awesomes', 'awesome-assesment' ),
-                    'search_items'       => __( 'Search Awesomes', 'awesome-assesment' ),
-                    'not_found'          => __( 'No awesomes found.', 'awesome-assesment' ),
-                    'not_found_in_trash' => __( 'No awesomes found in Trash.', 'awesome-assesment' )
-                ),
-                'public' => true,
-                'description' => __( 'Description of Awesome custom post type', 'awesome-assesment' ),
-                // Features the post type supports
-                'supports' => array( 'title', 'editor', 'author', 'thumbnail' ),
-            );
-            register_post_type('awesome', $args);
+            //use the lib provided in requirement to register custom post type
+            register_extended_post_type( 'awesome' );
         }
     }
